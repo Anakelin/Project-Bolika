52351e43447ced368d0b10636dcb1c0fcca126f9
Xu Jiaqi <jiaqi.xu3@studio.unibo.it>
2024-03-27 11:19:12 +0100

    Rebuilt node dependency

--- back-end/test.js
+++ back-end/test.js
@@ -1,2 +1,11 @@
 var msg = "Death";
 console.log(msg);
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: '',
+    database: 'testdata.ddl'
+})
