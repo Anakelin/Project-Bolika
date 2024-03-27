4a8df9072c4cf104e611e7526fa2531194e5b4c1
Xu Jiaqi <jiaqi.xu3@studio.unibo.it>
2024-03-27 11:33:12 +0100

    Moved package.json

--- back-end/test.js
+++ back-end/test.js
@@ -1,11 +1,4 @@
+// .\dependencies\node-v20.11.1-win-x64\npm init
+// .\\dependencies\\node-v20.11.1-win-x64\\node back-end/test.js
 var msg = "Death";
 console.log(msg);
-
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'user',
-    password: '',
-    database: 'testdata.ddl'
-})
