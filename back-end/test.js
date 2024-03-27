1ae64208f90c13cc9ce3b2bcad77ccee6d6c5909
Xu Jiaqi <jiaqi.xu3@studio.unibo.it>
2024-03-27 11:49:44 +0100

    Moved dependencies

--- back-end/test.js
+++ back-end/test.js
@@ -2,3 +2,14 @@
 // .\\dependencies\\node-v20.11.1-win-x64\\node back-end/test.js
 var msg = "Death";
 console.log(msg);
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '../front-end/sign_in.html');
+});
+
+app.use(express.static('public'));
